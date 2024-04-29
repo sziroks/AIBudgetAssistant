958f7795de4d8ec9bbe462df59eee543d0cc16b4
sziroks <smoczynskioskar@gmail.com>
2024-04-29 12:13:03 +0200

    Prepared for development

---
+++ ai_budget_assistant/budget_analysis/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BudgetAnalysisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'budget_analysis'
---
+++ ai_budget_assistant/budget_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BudgetManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'budget_manager'
