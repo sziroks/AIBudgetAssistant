4d8547b563e4de97c45c5c34afad576869e1ea50
sziroks <smoczynskioskar@gmail.com>
2024-04-29 20:31:18 +0200

    Started landing page

--- ai_budget_assistant/ai_budget_assistant/settings.py
+++ ai_budget_assistant/ai_budget_assistant/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = "ai_budget_assistant.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ai_budget_assistant/budget_manager/views.py
+++ ai_budget_assistant/budget_manager/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, "budget_manager/landing_page.html")
